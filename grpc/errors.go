19b2764dc623c1e7bc8b66a100bed5baeecbcac6
Mohammad Nasirifar <farnasirim@gmail.com>
2020-09-25 22:29:23 -0400

    Fix nil error return in errorChain

--- grpc/errors.go
+++ grpc/errors.go
@@ -17,7 +17,10 @@ type errorChain struct {
 }
 
 func (e *errorChain) Is(other error) bool {
-	return e.Error() == other.Error()
+	if e == other {
+		return true
+	}
+	return other != nil && e.Error() == other.Error()
 }
 
 func (e *errorChain) Error() string {
@@ -25,6 +28,9 @@ func (e *errorChain) Error() string {
 }
 
 func (e *errorChain) Unwrap() error {
+	if e.Next == nil {
+		return nil
+	}
 	return e.Next
 }
 
