2ad8b61d5491bb4d9593d04d189706dd280fdf4d
Mohammad Nasirifar <farnasirim@gmail.com>
2020-10-02 00:49:31 -0400

    Refactor rex cli to separate client creation and cli logic

--- cmd/rex/main.go
+++ cmd/rex/main.go
@@ -26,63 +26,16 @@ import (
 	rex_grpc "github.com/farnasirim/rex/grpc"
 )
 
-var pathToCACert string
-var pathToCert string
-var pathToKey string
-
-func readFileOrFatal(filepath string) []byte {
-	content, err := ioutil.ReadFile(filepath)
-	if err != nil {
-		log.Fatalf("Failed to read %s: %v\n", filepath, err)
-	}
-	return content
-}
+var (
+	pathToCACert string
+	pathToCert   string
+	pathToKey    string
+)
 
 func main() {
 	log.SetLevel(log.DebugLevel)
-
-	flag.StringVar(&pathToCACert, "ca", "", "path to ca certificate in pem format")
-	flag.StringVar(&pathToCert, "cert", "", "path to server certificate in pem format")
-	flag.StringVar(&pathToKey, "key", "", "path to server private key in pem format")
-
-	flag.Parse()
-
-	if pathToCACert == "" {
-		log.Fatalln("Missing -ca arg")
-	}
-
-	if pathToKey == "" {
-		log.Fatalln("Missing -key arg")
-	}
-
-	if pathToCert == "" {
-		log.Fatalln("Missing -cert arg")
-	}
-
-	caPool := x509.NewCertPool()
-	if ok := caPool.AppendCertsFromPEM(readFileOrFatal(pathToCACert)); !ok {
-		log.Fatalln("CA cert malformed")
-	}
-
-	cert, err := tls.LoadX509KeyPair(pathToCert, pathToKey)
-	if err != nil {
-		log.Fatalf("Failed to load key pair: %v\n", err)
-	}
-
-	config := &tls.Config{
-		MinVersion:         tls.VersionTLS13,
-		InsecureSkipVerify: false,
-		RootCAs:            caPool,
-		Certificates:       []tls.Certificate{cert},
-	}
-
-	tlsCredentials := credentials.NewTLS(config)
-	conn, err := grpc.Dial("localhost:9090",
-		grpc.WithTransportCredentials(tlsCredentials),
-		grpc.WithUnaryInterceptor(rex_grpc.ErrorUnmarshallerInterceptor))
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
+	parseAndValidate()
+	conn := getGRPCConnection()
 	defer func() {
 		if err := conn.Close(); err != nil {
 			log.Fatalln(err.Error())
@@ -91,7 +44,6 @@ func main() {
 
 	var client rex.Service = rex_grpc.NewClient(conn)
 
-	log.Debugln("Created new client")
 	if flag.NArg() < 1 {
 		log.Fatalln("missing action")
 	}
@@ -203,4 +155,61 @@ func main() {
 	default:
 		log.Fatalf("Invalid action: %q", action)
 	}
+}
+
+func parseAndValidate() {
+	flag.StringVar(&pathToCACert, "ca", "", "path to ca certificate in pem format")
+	flag.StringVar(&pathToCert, "cert", "", "path to server certificate in pem format")
+	flag.StringVar(&pathToKey, "key", "", "path to server private key in pem format")
+
+	flag.Parse()
+
+	if pathToCACert == "" {
+		log.Fatalln("Missing -ca arg")
+	}
+
+	if pathToKey == "" {
+		log.Fatalln("Missing -key arg")
+	}
+
+	if pathToCert == "" {
+		log.Fatalln("Missing -cert arg")
+	}
+
+}
+
+func readFileOrFatal(filepath string) []byte {
+	content, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		log.Fatalf("Failed to read %s: %v\n", filepath, err)
+	}
+	return content
+}
+
+func getGRPCConnection() *grpc.ClientConn {
+	caPool := x509.NewCertPool()
+	if ok := caPool.AppendCertsFromPEM(readFileOrFatal(pathToCACert)); !ok {
+		log.Fatalln("CA cert malformed")
+	}
+
+	cert, err := tls.LoadX509KeyPair(pathToCert, pathToKey)
+	if err != nil {
+		log.Fatalf("Failed to load key pair: %v\n", err)
+	}
+
+	config := &tls.Config{
+		MinVersion:         tls.VersionTLS13,
+		InsecureSkipVerify: false,
+		RootCAs:            caPool,
+		Certificates:       []tls.Certificate{cert},
+	}
+
+	tlsCredentials := credentials.NewTLS(config)
+	conn, err := grpc.Dial("localhost:9090",
+		grpc.WithTransportCredentials(tlsCredentials),
+		grpc.WithUnaryInterceptor(rex_grpc.ErrorUnmarshallerInterceptor))
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	return conn
 }
