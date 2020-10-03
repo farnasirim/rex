07ed4f00e5d344693289ca569fc69db14395a1fb
Mohammad Nasirifar <farnasirim@gmail.com>
2020-10-02 23:23:51 -0400

    Add tests for GRPC RBAC

--- grpc/rbac_test.go
+++ grpc/rbac_test.go
@@ -24,6 +24,23 @@ func TestSimpleAccessRule_MatchPrincipal_Wildcard_Reverse(t *testing.T) {
 	}
 }
 
-func TestPolicyEnforcer_Enforce(t *testing.T) {
+type policyMock struct {
+	EnforceFunc func(context.Context) (bool, bool)
+}
 
+func (m *policyMock) Enforce(ctx context.Context) (bool, bool) {
+	return m.EnforceFunc(ctx)
+}
+
+func TestPolicyEnforcer_Enforce_DefaultToDeny(t *testing.T) {
+	enforcer := NewPolicyEnforcer(
+		&policyMock{func(context.Context) (bool, bool) { return true, false }},
+		&policyMock{func(context.Context) (bool, bool) { return true, false }})
+
+	ctxUser := rex.WithUserID(context.Background(), "*")
+	ctxMethod := withMethodName(ctxUser, "/path/to/action")
+	verdict, applies := enforcer.Enforce(ctxMethod)
+	if verdict || applies {
+		t.Errorf("Rule must not apply. Was expecting (false, false), got (%v, %v)", verdict, applies)
+	}
 }
